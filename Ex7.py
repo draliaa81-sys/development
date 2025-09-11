99cb1952db1c073ed6184c48e5d92b765b60b34e
draliaa81-sys <dr.aliaa81@gmail.com>
2025-09-11 20:33:59 +1200

    Add files via upload
    week3

---
+++ Ex7.py
@@ -0,0 +1,6 @@
+# Create a list called 'color_list' containing color names
+color_list = ["Red", "Green", "White", "Black"]
+
+# Print the first and last elements of the 'color_list'
+print(f"First color:  {color_list[0]}")
+print(f"Last color: {color_list[-1]}")
---
+++ Ex8.py
@@ -0,0 +1,8 @@
+# import the 'calendar' module
+import calendar
+# promot the user to input the year and month
+y= int(input("input the year:"))
+m=int(input("input the month"))
+
+# print the calendar for the specified year and month
+print(calendar.month(y,m))
